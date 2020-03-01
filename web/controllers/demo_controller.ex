e633b5aa602e9f0de1f2b441cad4983f286d2d1c
Tilo Sloboda <tsloboda@chime.com>
2020-02-29 19:05:57 -0800

    adding custom route

---
+++ web/controllers/demo_controller.ex
@@ -0,0 +1,34 @@
+defmodule ElixirExample.DemoController do
+  use ElixirExample.Web, :controller
+  require Logger
+
+  def index(conn, _params) do
+    log(conn)
+    text conn, "Hello World from #{ip()} with foo as #{foo()} url: #{current_url(conn)}"
+  end
+
+  def echo(conn, _params) do
+    log(conn)
+    text conn, "loaderio-757ad3993948d06bd057710711dd9a66"
+  end
+
+
+  def log(conn) do
+    # url = ElixirExample.Router.Helpers.url(conn) <> conn.request_path
+    Logger.info  "Received request for URL: #{current_url(conn)}"
+  end
+
+  # returns a string with the IP address of eth0
+  defp ip do
+    {:ok, socket} =  :inet.getif()
+    [eth0 | _rest] = socket
+    {ip, _mask, _net} = eth0
+    Enum.join(Tuple.to_list(ip), ".")
+  end
+
+  # fetch value of ENV variable FOO
+  defp foo do
+    {:ok, value} = System.fetch_env("FOO")
+    value
+  end
+end
--- web/router.ex
+++ web/router.ex
@@ -13,7 +13,18 @@ defmodule ElixirExample.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :minimal do
+    plug :accepts, ["txt","html"]
+  end
+
   scope "/", ElixirExample do
+    pipe_through :minimal
+
+    get "/", DemoController, :index
+    get "/loaderio-757ad3993948d06bd057710711dd9a66.txt", DemoController, :echo
+  end
+
+  scope "/web", ElixirExample do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
