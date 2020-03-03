26bd09841481d6c0a582c64cfbd3849682ea370a
Tilo Sloboda <tsloboda@chime.com>
2020-03-03 01:09:04 -0800

    update

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :elixir_example, ElixirExample.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
-  url: [host: "localhost", port: 80],
+  url: [host: "localhost", port: 8080],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 MIX_ENV=prod mix compile.protocols
-MIX_ENV=prod PORT=80 elixir -pa _build/prod/consolidated -S mix phoenix.server
+MIX_ENV=prod PORT=8080 elixir -pa _build/prod/consolidated -S mix phoenix.server
